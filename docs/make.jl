ebd1decbd772e57bbbe9752798a0d9550cb836c8
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2025-09-19 16:07:45 -0700

    fix doc deploy

--- docs/make.jl
+++ docs/make.jl
@@ -3,32 +3,17 @@
 using Documenter, PyBayesOpt
 using Optim  # ensure Optim methods available during doc build
 
-is_ci = get(ENV, "CI", "false") == "true"
+makedocs(
+    modules = [PyBayesOpt],
+    format = Documenter.HTML(; prettyurls = true),
+    authors = "Jürgen Fuhrmann",
+    sitename = "PyBayesOpt.jl",
+    pages = Any["index.md", "internal.md"],
+    repo = "https://github.com/j-fu/PyBayesOpt.jl",
+    warnonly = [:missing_docs, :docs_block, :cross_references],
+)
 
-if is_ci
-    makedocs(
-        modules = [PyBayesOpt],
-        format = Documenter.HTML(; prettyurls = true),
-        authors = "Jürgen Fuhrmann",
-        sitename = "PyBayesOpt.jl",
-        pages = Any["index.md", "internal.md"],
-        repo = "https://github.com/j-fu/PyBayesOpt.jl",
-        warnonly = [:missing_docs, :docs_block, :cross_references],
-    )
-else
-    makedocs(
-        modules = [PyBayesOpt],
-        format = Documenter.HTML(; prettyurls = false, edit_link = nothing),
-        authors = "Jürgen Fuhrmann",
-        sitename = "PyBayesOpt.jl",
-        pages = Any["index.md", "internal.md"],
-        repo = "local", # dummy
-        remotes = Dict(),
-        warnonly = [:missing_docs, :docs_block, :cross_references],
-    )
-end
-
-if is_ci
+if !isinteractive()
     deploydocs(
         repo = "github.com/j-fu/PyBayesOpt.jl.git",
         push_preview = true
